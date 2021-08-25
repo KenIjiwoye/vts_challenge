cd85b94fd0d64905556f75329bfc01bb268d0d0a
KenIjiwoye <alistluxury@gmail.com>
2021-08-25 00:27:04 +0000

    intergrated react hook forms

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,32 +3,41 @@ import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    email: string,
+    password: string,
+  };
 
 const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
     return (
         <Container style={{ width: '500px'}} >
             <h2>Login</h2>
              <Card>
                  <Card.Body>
-                    <Form>
+                    <Form onSubmit ={handleSubmit(onSubmit)} >
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter an email address..." />
+                            <Form.Control type="email" placeholder="Enter an email address..." {...register("email", {required: true})} />
                             <Form.Text className="text-muted">
                             Ex. demo@test.com
                             </Form.Text>
+                            {errors.email && <span style={{color:'red'}} >This field is required</span>}
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="email" placeholder="Enter your password..." />
+                            <Form.Control type="password" placeholder="Enter your password..." {...register("password", {required: true})} />
                             <Form.Text className="text-muted">
                             Ex. password1
                             </Form.Text>
+                            {errors.password && <span style={{color:'red'}} >This field is required</span>}
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Login
-                        </Button>
+                        <Form.Control type='submit' value='Login' className='btn btn-primary' />
+
                     </Form>
                  </Card.Body>
              </Card>
